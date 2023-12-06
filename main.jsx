2efe7bfc257a8287dd0edca4e5c45ef5fffa90c2
AnanyaGodse <143485276+AnanyaGodse@users.noreply.github.com>
2023-12-06 22:31:00 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoginDialog from "./components/LoginDialog";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LoginDialog />
+  </React.StrictMode>
+);
